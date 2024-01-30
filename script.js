39d1822322505180bbbc17c281ac6f2d45667531
JHale3 <learnspark3@gmail.com>
2024-01-30 12:25:16 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const cars = [
+  { make: 'Toyota', yrsOld: 5, mileage: 92399 },
+  { make: 'Ford', yrsOld: 12, mileage: 255005 },
+  { make: 'Ferrari', yrsOld: 9, mileage: 12966 },
+  { make: 'Subaru', yrsOld: 9, mileage: 111266 },
+  { make: 'Toyota', yrsOld: 2, mileage: 41888 },
+  { make: 'Tesla', yrsOld: 3, mileage: 57720 }
+];
+
+
+const wellDrivenCars = cars.filter(car => (car.mileage / car.yrsOld) > 20000);
+
+
+wellDrivenCars.forEach(car => {
+  console.log(car);
+});
